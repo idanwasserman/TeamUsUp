b9248d2bcbdb924b09c7230c01128d719424c8bb
idanwasserman <idan.wasserman@s.afeka.ac.il>
2022-01-20 18:04:24 +0200

    Instance service added

---
+++ app/src/main/java/com/idan/teamusup/logic/InstanceServiceImpl.java
@@ -0,0 +1,159 @@
+package com.idan.teamusup.logic;
+
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.idan.teamusup.data.Constants;
+import com.idan.teamusup.services.FirebaseRealtimeDB;
+import com.idan.teamusup.data.Instance;
+import com.idan.teamusup.data.InstanceType;
+import com.idan.teamusup.data.Location;
+import com.idan.teamusup.services.MySharedPreferences;
+import com.idan.teamusup.services.UserDatabase;
+import com.idan.teamusup.logic.interfaces.InstanceService;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class InstanceServiceImpl implements InstanceService {
+
+    private static final String TAG = "InstanceServiceImpl_TAG";
+
+    @Override
+    public Instance createInstance(String userId, Instance instance) {
+        instance.setId(UUID.randomUUID().toString());
+        if (instance.getCreatedTimestamp() == null) {
+            instance.setCreatedTimestamp(new Date());
+        }
+        instance.setCreatedByUserId(userId);
+
+        // save instance in user database
+        UserDatabase.getDatabase().addInstance(instance);
+
+        return instance;
+    }
+
+    @Override
+    public List<Instance> getAllInstancesByType(String type) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            return UserDatabase
+                    .getDatabase()
+                    .getInstances()
+                    .stream()
+                    .filter(i -> i.getType().name().equals(type))
+                    .collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    @Override
+    public void updateUserLocation(Instance userInstance) {
+        UserDatabase.getDatabase().getUser().setLocation(new Location(
+                userInstance.getLocation().getLat(),
+                userInstance.getLocation().getLng()));
+        FirebaseRealtimeDB.getRealtimeDB().updateUserLocation(userInstance);
+    }
+
+    @Override
+    public void saveData() {
+        UserDatabase database = UserDatabase.getDatabase();
+        Instance user = database.getUser();
+        String userId = user.getId();
+
+        String userJson = new Gson().toJson(user);
+        String instancesJson = new Gson().toJson(database.getInstances());
+
+        MySharedPreferences.getInstance().putString(
+                createKey(userId, Constants.user.name()), userJson);
+        MySharedPreferences.getInstance().putString(
+                createKey(userId, Constants.instances.name()), instancesJson);
+    }
+
+    @Override
+    public Instance getUserInstance(FirebaseUser user) {
+        String userJson = MySharedPreferences.getInstance().getString(
+                createKey(user.getUid(), Constants.user.name()), null);
+        if (userJson == null) {
+            Log.d(TAG, "Creates new user instance");
+            return createNewUser(user);
+        } else {
+            return new Gson().fromJson(userJson, Instance.class);
+        }
+    }
+
+    @Override
+    public List<Instance> getDatabaseInstances(FirebaseUser user) {
+        String instancesJson = MySharedPreferences.getInstance().getString(
+                createKey(user.getUid(), Constants.instances.name()), null);
+        if (instancesJson == null) {
+            return new ArrayList<>();
+        } else {
+            return new Gson().fromJson(
+                    instancesJson, new TypeToken<ArrayList<Instance>>(){}.getType());
+        }
+    }
+
+    private Instance createNewUser(FirebaseUser user) {
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put(Constants.photoUrl.name(), getPhotoUrl(user));
+        attributes.put(Constants.isNew.name(), true);
+
+        return new Instance()
+                .setCreatedTimestamp(new Date())
+                .setId(user.getUid())
+                .setType(InstanceType.User)
+                .setName(getDisplayName(user))
+                .setCreatedByUserId(Constants.ADMIN.name())
+                .setAttributes(attributes);
+    }
+
+    private String createKey(String id, String str) {
+        final String DELIMITER = "&&";
+        return (id + DELIMITER + str);
+    }
+
+    private String getDisplayName(FirebaseUser user) {
+        String displayName = user.getDisplayName();
+        if (displayName != null && !displayName.isEmpty()) {
+            return displayName;
+        }
+
+        String email = user.getEmail();
+        if (email != null && !email.isEmpty()) {
+            return email;
+        }
+
+        String phoneNumber = user.getPhoneNumber();
+        if (phoneNumber != null && !phoneNumber.isEmpty()) {
+            return phoneNumber;
+        }
+
+        return "Empty username";
+    }
+
+    private String getPhotoUrl(FirebaseUser user) {
+        if (user.getPhotoUrl() == null) return "";
+        StringBuilder url = new StringBuilder(user.getPhotoUrl().toString());
+        for (UserInfo userInfo : user.getProviderData()) {
+            if (userInfo.getProviderId().equals("facebook.com")) {
+                if (AccessToken.getCurrentAccessToken() != null) {
+                    url.append("?access_token=").append(
+                            Objects.requireNonNull(
+                                    AccessToken.getCurrentAccessToken()).getToken());
+                }
+            }
+        }
+        return url.toString();
+    }
+
+}
---
+++ app/src/main/java/com/idan/teamusup/logic/interfaces/InstanceService.java
@@ -0,0 +1,21 @@
+package com.idan.teamusup.logic.interfaces;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.idan.teamusup.data.Instance;
+
+import java.util.List;
+
+public interface InstanceService {
+
+    Instance createInstance(String userId, Instance instance);
+
+    List<Instance> getAllInstancesByType(String type);
+
+    void updateUserLocation(Instance userInstance);
+
+    void saveData();
+
+    Instance getUserInstance(FirebaseUser user);
+
+    List<Instance> getDatabaseInstances(FirebaseUser user);
+}
