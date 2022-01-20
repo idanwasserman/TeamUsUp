ded9c57e9f074981eb42bec26ee87e21471cf941
idanwasserman <idan.wasserman@s.afeka.ac.il>
2022-01-20 17:06:55 +0200

    My shared preferences class added

---
+++ app/src/main/java/com/idan/teamusup/data/MySharedPreferences.java
@@ -0,0 +1,36 @@
+package com.idan.teamusup.data;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MySharedPreferences {
+
+    private static MySharedPreferences instance;
+    private SharedPreferences sharedPreferences;
+
+    public static MySharedPreferences getInstance() {
+        return instance;
+    }
+
+    private MySharedPreferences(Context context, String filename) {
+        sharedPreferences = context
+                .getApplicationContext()
+                .getSharedPreferences(filename, Context.MODE_PRIVATE);
+    }
+
+    public static MySharedPreferences init(Context context, String filename) {
+        if (instance == null) {
+            instance = new MySharedPreferences(context, filename);
+        }
+        return instance;
+    }
+
+    public String getString(String key, String defaultValue) {
+        return sharedPreferences.getString(key, defaultValue);
+    }
+
+    public void putString(String key, String value) {
+        sharedPreferences.edit().putString(key, value).apply();
+    }
+
+}
